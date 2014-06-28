bfb0c5e9ba4518fce4c80c3ca90da26d2b1385cb
Bokodi <dr.bokgabor@gmail.com>
2014-06-28 14:41:29 +0200

    ugly code but better performance

--- mazeGenerator.js
+++ mazeGenerator.js
@@ -26,33 +26,38 @@ window.onload = function init() {
 		maze.height = Maze.rows * Maze.block;
 
 		var mazeList = [],
+			mazeIndexArr = [],
 			frontierCells = [],
+			frontierIndexArr = [],
 			c = {
 				x: Math.floor(Math.random() * Maze.cols),
 				y: Math.floor(Math.random() * Maze.rows),
 				walls: [1, 1]
 			},
-			index, tempC, points, point, mazeItem;
+			len = Maze.dirs.length,
+			index, points, point, mazeItem;
 		
 		do {
-			mazeList.push(c);
-			for (var i = 0, il = Maze.dirs.length; i < il; i++) {
+			index = c.y * Maze.cols + c.x;
+			mazeList[index] = c;
+			mazeIndexArr.push(index);
+			for (var i = 0, tempC, tempIndex; i < len; i++) {
 				tempC = { x: c.x + Maze.dirs[i][0], y: c.y + Maze.dirs[i][1], walls: [1, 1] };
-				if (!myFilter(frontierCells, mazeList, tempC) && tempC.x >= 0 && tempC.x < Maze.cols && tempC.y >= 0 && tempC.y < Maze.rows) {
-					frontierCells.push(tempC);
+				tempIndex = tempC.y * Maze.cols + tempC.x;
+				if (!mazeList[tempIndex] && !frontierCells[tempIndex] && tempC.x >= 0 && tempC.x < Maze.cols && tempC.y >= 0 && tempC.y < Maze.rows) {
+					frontierCells[tempIndex] = tempC;
+					frontierIndexArr.push(tempIndex);
 				}
 			}
-			if ((index = frontierCells.indexOf(c)) != -1) {
-				frontierCells.splice(index, 1);
+			if (frontierCells[index]) {
+				delete frontierCells[index];
+				frontierIndexArr.splice(frontierIndexArr.indexOf(index), 1);
 			}
-			c = frontierCells[Math.floor(Math.random() * frontierCells.length)];
-			if (c) {
+			if (c = frontierCells[frontierIndexArr[Math.floor(Math.random() * frontierIndexArr.length)]]) {
 				points = [];
-				for (var j = 0, jl = mazeList.length, ji; j < jl; j++) {
-					ji = mazeList[j];
-					if (Math.abs(ji.x - c.x) == 1 && Math.abs(ji.y - c.y) == 0 ||
-						Math.abs(ji.x - c.x) == 0 && Math.abs(ji.y - c.y) == 1) {
-							points.push(ji);
+				for (var j = 0, ji; j < len; j++) {
+					if (ji = mazeList[(c.y + Maze.dirs[j][1]) * Maze.cols + c.x + Maze.dirs[j][0]]) {
+						points.push(ji);
 					}
 				}
 				point = points[Math.floor(Math.random() * points.length)];
@@ -62,13 +67,13 @@ window.onload = function init() {
 					point.x > c.x ? point.walls[1] = 0 : c.walls[1] = 0
 				;
 			}
-		} while (frontierCells.length)
+		} while (frontierIndexArr.length)
 		
 		ctx.beginPath();
 		ctx.lineWidth = 0.5;
 		ctx.strokeStyle = '#222222';
-		for (var i = 0, il = mazeList.length; i < il; i++) {
-			mazeItem = mazeList[i];
+		for (var i = 0, il = mazeIndexArr.length; i < il; i++) {
+			mazeItem = mazeList[mazeIndexArr[i]];
 			if (mazeItem.walls[0] && mazeItem.y != 0) {
 				ctx.moveTo(mazeItem.x * Maze.block, mazeItem.y * Maze.block);
 				ctx.lineTo(mazeItem.x * Maze.block + Maze.block, mazeItem.y * Maze.block);
@@ -82,27 +87,6 @@ window.onload = function init() {
 		console.timeEnd('s');
 		settings.click();
 	});
-}
-
-// native array.prototype.filter ~10 times slower
-function myFilter(frontierArr, mazeArr, item) {
-	var matched = [];
-	for (var j = 0, jl = frontierArr.length, ji; j < jl; j++) {
-		ji = frontierArr[j];
-		if (ji.x == item.x && ji.y == item.y) {
-			matched.push(ji);
-		}
-	}
-	if (matched.length) {
-		return true;
-	}
-	for (var j = 0, jl = mazeArr.length, ji; j < jl; j++) {
-		ji = mazeArr[j];
-		if (ji.x == item.x && ji.y == item.y) {
-			matched.push(ji);
-		}
-	}
-	return matched.length > 0;
 }
 
 function id(id) {
